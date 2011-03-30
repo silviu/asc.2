1277e5b0a72d1f6dcaa9be135d9ae090f9002f67
Silviu Grijincu <silviu.grijincu@gmail.com>
2011-03-30 19:33:00 +0300

    init matrixes for 1

--- matrix.c
+++ matrix.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+#define DIM 787
+unsigned long long a0[DIM][DIM], a1[DIM][DIM], a2[DIM][DIM],
+                   b0[DIM][DIM], b1[DIM][DIM], b2[DIM][DIM];
+
+void init()
 {
-  printf("Hello\n");
-  return 0;
+    unsigned int i, j;
+    for (i = 0; i < DIM; i++)
+		for (j = 0; j < DIM; j++) {
+            a0[i][j] = (i + j + 2) % 10000;
+            a1[i][j] = (2 * (i + j + 2)) % 10000;
+            a2[i][j] = (3 * (i + j + 2)) % 10000;
+            
+            b0[i][j] = ((i + 1) * (j + 5)) % 10000;
+            b1[i][j] = ((i + 2) * (j + 5)) % 10000;
+            b2[i][j] = ((i + 3) * (j + 5)) % 10000;
+        }
+ }
+
+
+int main(int argc, char** argv)
+{
+    init();
+    return 0;
 }
