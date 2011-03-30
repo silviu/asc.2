8ec926335520c863ff2ea2ebedaf53a9911c02e5
Silviu Grijincu <silviu.grijincu@gmail.com>
2011-03-30 22:15:35 +0300

    2 gata. de la 1min39sec la 50 de sec

--- matrix.c
+++ matrix.c
@@ -54,7 +54,75 @@ void first()
             mrez[i][j] = c[0][i][j] ^ c[1][i][j] ^ c[2][i][j] ^ c[3][i][j] ^ c[4][i][j];
 }
 
-void first_output(const char* filename)
+
+void second()
+{
+    int i, j, k;
+    for (i = 0; i < DIM; i++) {
+        unsigned long long* orig_line_a0 = &a[0][i][0];
+        unsigned long long* orig_line_a1 = &a[1][i][0];
+        unsigned long long* orig_line_a2 = &a[2][i][0];
+        
+        for (j = 0; j < DIM; j++) {
+            unsigned long long* line_a0 = orig_line_a0;
+            unsigned long long* line_a1 = orig_line_a1;
+            unsigned long long* line_a2 = orig_line_a2;
+        
+            unsigned long long* col_b0 = &b[0][0][j];
+            unsigned long long* col_b1 = &b[1][0][j];
+            unsigned long long* col_b2 = &b[2][0][j];
+            
+            register unsigned long long s00, s01, s02, s10, s11, s12, s20, s21, s22;
+            s00 = 0; s01 = 0; s02 = 0; s10= 0; s11 = 0; s12 = 0; s20 = 0; s21 = 0; s22 = 0;
+            for (k = 0; k < DIM; k++) {
+                s00 += *line_a0 * *col_b0;
+                s01 += *line_a0 * *col_b1;
+                s02 += *line_a0 * *col_b2;
+                
+                s10 += *line_a1 * *col_b0;
+                s11 += *line_a1 * *col_b1;
+                s12 += *line_a1 * *col_b2;
+                
+                s20 += *line_a2 * *col_b0;
+                s21 += *line_a2 * *col_b1;
+                s22 += *line_a2 * *col_b2;
+                
+                line_a0++;
+                line_a1++;
+                line_a2++;
+                
+                col_b0 += DIM;
+                col_b1 += DIM;
+                col_b2 += DIM;
+            }
+            a0b0[i][j] = s00;
+            a0b1[i][j] = s01;
+            a0b2[i][j] = s02;
+            
+            a1b0[i][j] = s10;
+            a1b1[i][j] = s11;
+            a1b2[i][j] = s12;
+            
+            a2b0[i][j] = s20;
+            a2b1[i][j] = s21;
+            a2b2[i][j] = s22;
+        }
+    }
+
+    for (i = 0; i < DIM; i++)
+        for (j = 0; j < DIM; j++) {
+            c[0][i][j] = a0b0[i][j];
+            c[1][i][j] = a0b1[i][j] + a1b0[i][j];
+            c[2][i][j] = a0b2[i][j] + a1b1[i][j] + a2b0[i][j];
+            c[3][i][j] = a1b2[i][j] + a2b1[i][j];
+            c[4][i][j] = a2b2[i][j];
+        }
+    for (i = 0; i < DIM; i++)
+        for (j = 0; j < DIM; j++)
+            mrez[i][j] = c[0][i][j] ^ c[1][i][j] ^ c[2][i][j] ^ c[3][i][j] ^ c[4][i][j];
+}
+
+void output(const char* filename)
 {
     int i, j;
     
@@ -87,7 +155,8 @@ void first_output(const char* filename)
 int main(int argc, char** argv)
 {
     init();
-    first();
-    first_output(argv[1]);
+    //first();
+    second();
+    output(argv[1]);
     return 0;
 }
